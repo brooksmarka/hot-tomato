047bc6c98249c7770ebfc713e03b6224691a7234
Mark Brooks <Markabrooks@gmail.com>
2017-11-10 21:40:32 -0700

    Checkpoint 2 Start and Reset: Complete

--- app/scripts/controllers/TimerCtrl.js
+++ app/scripts/controllers/TimerCtrl.js
@@ -1,46 +1,40 @@
 (function() {
 
     function TimerCtrl($scope, $interval) {
-      this.startButton = "Start a Session";
-      this.resetButton = "Reset Session";
-      this.breakButton = "Take a Break";
+      $scope.buttonMsg = "Start a Session";
       $scope.time = 1500;
-      console.log("I hear you!")
-
-      $scope.onBreak = false; //boolean value to trigger button change
-      var stop;
+      $scope.format = 'mm:ss';
+      console.log("TimerCtrl Hears you!")
 
       var timer;
 
-      var countdown = function() {
-        $scope.time = $scope.time - 1000
-
-        if($scope.time <= 0) {
-          $interval.cancel(timer);
-        }
-      }
+      $scope.timerTrigger = function() {
+          console.log("This is timer trigger!")
+          if( $scope.buttonMsg === "Reset Session" ) {
+          resetSession();
+          }else{
+          $scope.session();
+          }
+        };
 
       $scope.session = function() {
-        timer = $interval(countdown, 2000)
-      };
+          timer = $interval(countdown, 1000) // this is the delay
+        };
 
-      $scope.stopSession = function() {
-        //making sure a session is active
-        if (angular.isDefined(stop)) {
-          $interval.cancel(stop);
-          stop = undefined;
+      var countdown = function() {
+        $scope.time = $scope.time - 1
+        $scope.buttonMsg = "Reset Session";
+        if($scope.time <= 0) {
+          resetSession();
         }
       };
 
-      $scope.resetSession = function() {
-        console.log("I hear you too!")
-        $scope.count = 1500;
+      var resetSession = function() {
+        console.log("This is resetSession!")
+        $interval.cancel(timer);
+        $scope.time = 1500;
+        $scope.buttonMsg = "Start a Session"
       };
-
-      $scope.$on('$destroy', function(){
-        //Make sure that the interval is destroyed as well
-        $scope.stopSession();
-      });
 
     };
 
